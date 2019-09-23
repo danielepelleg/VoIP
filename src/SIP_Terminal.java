ab946ab9bfe15434b26f33e736625c052a7532e2
tiaringhio <37662752+tiaringhio@users.noreply.github.com>
2019-09-23 12:26:40 +0200

    Initial Commit

---
+++ src/SIP_Terminal.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.net.Inet4Address;
+
+public class SIP_Terminal {
+    Inet4Address userAddress;
+    int userPort;
+
+    public String Invite() {
+        return null;
+    }
+}
