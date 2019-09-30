6ff173df78b2a502ea73017fcc3489748715d571
danielepelleg <55688016+danielepelleg@users.noreply.github.com>
2019-09-30 11:03:34 +0200

    Complete Switch with 4xx Response.
    The switch now reads the last 3 byte of message and gives back the type of response (whether it is an error) and its description. Tested and working .
    (ex. try to not respond on call)

--- src/main/java/UserAgent.java
+++ src/main/java/UserAgent.java
@@ -18,7 +18,6 @@ public class UserAgent {
 
         String bye = Files.readString(Paths.get("ack.txt"), StandardCharsets.UTF_8);
 
-        // TODO ACK
         // TODO Error management on received packages
 
         //take the invite message and convert it in bytes
@@ -60,6 +59,7 @@ public class UserAgent {
             serveAnswer = new String(serveAnswerB);
         }while(serveAnswer.charAt(0) == '1');
 
+        // Switch that gives the type of response (whether it is an error) and its description.
         switch (serveAnswer.charAt(0)){
             case '2':
                 send = ack.getBytes();
@@ -68,9 +68,87 @@ public class UserAgent {
                 break;
 
             case '3':
+
                 break;
 
             case '4':
+                switch (serveAnswer.charAt(1)) {
+                    case '0':
+                        switch (serveAnswer.charAt(2)) {
+                            case '0': //400 The request could not be understood due to malformed syntax.
+                                System.out.println(serveAnswer + " BAD REQUEST");
+                                break;
+                            case '1': //401 The request requires user authentication. This response is issued by UASs and registrars.
+                                System.out.println(serveAnswer + " UNAUTHORIZED");
+                                break;
+                            case '3': //403 The server understood the request, but is refusing to fulfill it. Sometimes the call has been rejected by the receiver.
+                                System.out.println(serveAnswer + " FORBIDDEN");
+                                break;
+                            case '4': //404 The server has definitive information that the user does not exist at the domain specified in the Request-URI.
+                                System.out.println(serveAnswer + " NOT FOUND");
+                                break;
+                            case '5': //405 The method specified in the Request-Line is understood, but not allowed for the address identified by the Request-URI.
+                                System.out.println(serveAnswer + " METHOD NON ALLOWED");
+                                break;
+                            case '6': //406 The server doesn't accept the header field.
+                                System.out.println(serveAnswer + " NOT ACCEPTABLE");
+                                break;
+                            case '8': //408 Couldn't find the user in time.
+                                System.out.println(serveAnswer + " REQUEST TIMEOUT");
+                                break;
+                            case '9': //409 User already registered.
+                                System.out.println(serveAnswer + " CONFLICT");
+                                break;
+                        }
+                        break;
+                    case '1':
+                        switch (serveAnswer.charAt(2)) {
+                            case '0': //410 The user existed once, but is not available here any more.
+                                System.out.println(serveAnswer + " GONE");
+                                break;
+                            case '5': //415 Request body in a format not supported.
+                                System.out.println(serveAnswer + " UNSUPPORTED MEDIA TYPE");
+                                break;
+                        }
+                        break;
+                    case '2':
+                        switch (serveAnswer.charAt(2)) {
+                            case '0': //420 Bad SIP Protocol Extension used, not understood by the server.
+                                System.out.println(serveAnswer + " GONE");
+                                break;
+                        }
+                        break;
+                    case '8':
+                        switch (serveAnswer.charAt(2)) {
+                            case '0': //480 Callee currently unavailable.
+                                System.out.println(serveAnswer + " TEMPORARILY UNAVAILABLE");
+                                break;
+                            case '3': //483 Max-Forwards header has reached the value '0'.
+                                System.out.println(serveAnswer + " TOO MANY HOPS");
+                                break;
+                            case '6': //486 Callee is busy.
+                                System.out.println(serveAnswer + " BUSY HERE");
+                                break;
+                            case '7': //487 Request has terminated by bye or cancel.
+                                System.out.println(serveAnswer + " REQUEST TERMINATED");
+                                break;
+                            case '8': //488 Some aspect of the session description or the Request-URI is not acceptable
+                                System.out.println(serveAnswer + " NOT ACCEPTABLE HERE");
+                                break;
+                            case '9': //489 The server did not understand an event package specified in an Event header field.
+                                System.out.println(serveAnswer + " BAD EVENT");
+                                break;
+                        }
+                    case '9':
+                        switch (serveAnswer.charAt(2)) {
+                            case '1': //491 Server has some pending request from the same dialog.
+                                System.out.println(serveAnswer + " REQUEST PENDING");
+                                break;
+                        }
+                        break;
+
+
+                }
                 break;
 
             case '5':
